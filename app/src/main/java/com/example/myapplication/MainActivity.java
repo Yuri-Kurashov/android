1f9f5fcac41d5c6b2f4c33f0d21875b1cd0ba0f3
Yury Kurashov <yurikurashov@gmail.com>
2019-03-20 20:52:20 +0300

    testihg_find_id

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView myTextView = (TextView) findViewById(R.id.myText);
+        myTextView.setText("Android is cool!");
+        Button myBtn = (Button) findViewById(R.id.myBtn);
+        myBtn.setText("Start");
+        myBtn.setEnabled(false);
+        CheckBox myChb = (CheckBox) findViewById(R.id.myChb);
+        myChb.setChecked(true);
     }
 }
