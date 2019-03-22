47ff8a4c936f4fd6bda75b07087e2b303148b0c2
Yury Kurashov <yurikurashov@gmail.com>
2019-03-23 00:23:24 +0300

    onclic

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,23 +1,41 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+TextView textView;
+Button button1;
+Button button2;
+Button button3;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView myTextView = (TextView) findViewById(R.id.myText);
-        myTextView.setText("Android is cool!");
-        Button myBtn = (Button) findViewById(R.id.myBtn);
-        myBtn.setText("Start");
-        myBtn.setEnabled(false);
-        CheckBox myChb = (CheckBox) findViewById(R.id.myChb);
-        myChb.setChecked(true);
+        textView = (TextView)findViewById(R.id.textView);
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        button3 = (Button)findViewById(R.id.button3);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Pushed button 1");
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        
     }
 }
