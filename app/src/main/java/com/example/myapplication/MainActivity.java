9d15e6b9defd7dea181dbaf2dd10f3cd33644799
Yury Kurashov <yurikurashov@gmail.com>
2019-03-23 20:20:06 +0300

    res_testing

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,53 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-TextView textView;
-Button button1;
-Button button2;
-Button button3;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.textView);
-        button1 = findViewById(R.id.button1);
-        button2 = findViewById(R.id.button2);
-        button3 = findViewById(R.id.button3);
-
-        View.OnClickListener onClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                switch (view.getId()) {
-                    case R.id.button1:
-                        textView.setText(R.string.text1);
-                        break;
-                    case R.id.button2:
-                        textView.setText(R.string.text2);
-                        break;
-                    case R.id.button3:
-                        textView.setText(R.string.text3);
-                        break;
-                }
-            }
-
-        };
-        button1.setOnClickListener(onClickListener);
-        button2.setOnClickListener(onClickListener);
-        button3.setOnClickListener(onClickListener);
-
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                button3.setText(R.string.textButton3);
-            }
-        });
-
 
 
 
