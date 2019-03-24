6812e9ba563042ce4930247454969e922859fcfc
Yury Kurashov <yurikurashov@gmail.com>
2019-03-24 21:28:41 +0300

    try_onClick

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,14 +12,36 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
+Button button1;
+Button button2;
+TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button1 = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        text = findViewById(R.id.textView);
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.button:
+                        text.setText(R.string.Pushed_back);
+                        break;
+                    case R.id.button2:
+                        text.setText(R.string.Pushed_change);
+                        break;
+
+                }
+            }
+
+        };
+        button1.setOnClickListener(onClickListener);
+        button2.setOnClickListener(onClickListener);
 
 
     }
