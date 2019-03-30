e03861566310dd1b379b41e513cb568fdfc98686
Yury Kurashov <yurikurashov@gmail.com>
2019-03-30 18:34:01 +0300

    Vert_and_Hor_2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     Button button1;
     Button button2;
     TextView textView1;
-
+    ImageView imageView1;
 
 
     @Override
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         button1 = findViewById(R.id.button);
         button2 = findViewById(R.id.button2);
         textView1 = findViewById(R.id.textView);
+        imageView1 = findViewById(R.id.imageView);
 
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
@@ -37,13 +38,14 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.button2:
                         textView1.setText(R.string.cool);
                         break;
+                    case R.id.imageView:
+                        textView1.setText(R.string.just_text);
                 }
-
             }
         };
+
         button1.setOnClickListener(onClickListener);
         button2.setOnClickListener(onClickListener);
-
+        imageView1.setOnClickListener(onClickListener);
     }
-
 }
